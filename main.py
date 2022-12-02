42a227c1f24cd840af5e5574629e4633b30e5d30
niye8576 <niye8576@uni.sydney.edu.au>
2022-12-02 13:22:33 +1100

    Highest calorie total

--- main.py
+++ main.py
@@ -12,9 +12,22 @@ def file_reader():
             single_elf = [] 
         else:
             line = line.strip()
-            single_elf.append(line)
+            single_elf.append(int(line))
 
     f.close()
     return elves_data
 
+def get_highest_elf(data_list):
+    highest = 0 
+    
+    for item in data_list:
+        total = sum(item)
+        if total > highest:
+            highest = total 
+    
+    return highest
+     
 cleaned_data = file_reader()
+highest_value = get_highest_elf(cleaned_data)
+
+print(f"The highest calorie count is: {highest_value}")
